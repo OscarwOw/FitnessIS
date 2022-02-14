689f19227ff7ad1862ac9c9d28dccfe0d6424a24
OscarwOw <OscarwOw210@gmail.com>
2022-02-14 14:14:45 +0100

    Layout wiring

---
+++ FitnessIS/Controllers/TagController.cs
@@ -0,0 +1,19 @@
+﻿using FitnessIS.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessIS.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly DatabaseContext _dbContext;
+        public TagController(DatabaseContext dbContext)
+        {
+            _dbContext= dbContext;
+        }
+        public IActionResult Index()
+        {
+            var tags = _dbContext.tags.ToList();
+            return View();
+        }
+    }
+}
