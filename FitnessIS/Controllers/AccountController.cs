80c444a75dc11381d51390752eaa097b36d65311
OscarwOw <OscarwOw210@gmail.com>
2022-02-15 11:02:42 +0100

    something

--- FitnessIS/Controllers/AccountController.cs
+++ FitnessIS/Controllers/AccountController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FitnessIS.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FitnessIS.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DatabaseContext _dbContext;
+        public AccountController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+  
         public IActionResult Index()
         {
             return View();
