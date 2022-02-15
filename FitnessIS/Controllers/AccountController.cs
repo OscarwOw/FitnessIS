0cff7a4430d774448df74efa0fd54dbc9e713598
OscarwOw <OscarwOw210@gmail.com>
2022-02-15 10:19:45 +0100

    AddAcountController

---
+++ FitnessIS/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessIS.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
